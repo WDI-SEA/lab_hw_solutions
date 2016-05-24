4ede79d6800d17b15b2e095b9b227fc1db09e375
srobertson421 <srobertson421@gmail.com>
2016-05-24 11:57:38 -0700

    Added timeout to change value based on DOM

--- fruits_and_veggies_sean/starter-code/js/app.js
+++ fruits_and_veggies_sean/starter-code/js/app.js
@@ -1,9 +1,10 @@
 var fruitApp = angular.module('fruitVegApp', []);
 
-fruitApp.controller('fruitVegController', ['$scope', function($scope) {
+fruitApp.controller('fruitVegController', ['$scope', '$timeout', function($scope, $timeout) {
   $scope.mixed = fruit.concat(vegetables).sort(function() { return 0.5 - Math.random() });
   $scope.fruits = [];
   $scope.veggies = [];
+  $scope.incorrectItems = document.querySelectorAll('.incorrect').length;
   $scope.originals = {
     veggies: vegetables,
     fruits: fruit
@@ -12,14 +13,19 @@ fruitApp.controller('fruitVegController', ['$scope', function($scope) {
   $scope.move = function(name, originCol, destCol) {
     $scope[originCol].splice($scope[originCol].indexOf(name), 1);
     $scope[destCol].push(name);
-    $scope.incorrectItems = document.querySelectorAll('.incorrect').length;
     if($scope.mixed.length === 0) {
       $scope.checkForWin();
     }
+
+    // Using timeout to allow DOM to render
+    // before reseting $scope variable.
+    // $timeout calls $digest as well.
+    $timeout(function() {
+      $scope.incorrectItems = document.querySelectorAll('.incorrect').length;
+    }, 100);
   }
 
   $scope.checkItem = function(name, columnName) {
-
     if($scope.originals[columnName].indexOf(name) === -1) {
       return false;
     } else {
